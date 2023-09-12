3806679d3373f11bc2f45d5313d2053974ccf32a
ABDULLAHI <Ashleyalvins010@gmail.com>
2023-09-12 23:32:28 +0100

    Added emial.js

---
+++ Back-end/routes/utills/email.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = option => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth:{
+            user: 'isihaqabdullahi01@gmail.com',
+            pass: 'mfhjloaeznxnohqa'
+        }
+});
+
+const mailoption = {
+    from: 'SkyShowng  <abdullahi>',
+    to: option.email,
+    subject: option.subject,
+    text: option.message,
+    };
+
+
+    transporter.sendMail(mailoption, function(error, info) {
+        if(error){
+            console.log(error)
+        }else {
+            console.log('Email sent' +info.response)
+        }
+    });
+}
+
+module.exports= sendEmail
